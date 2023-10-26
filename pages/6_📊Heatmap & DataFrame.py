3566c4c029906dfbe70008980bccfb38a41672be
seonguk0893 <145532557+seonguk0893@users.noreply.github.com>
2023-10-26 10:29:23 +0900

    Update 6_📊Heatmap & DataFrame.py

--- pages/6_📊Heatmap & DataFrame.py
+++ pages/6_📊Heatmap & DataFrame.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import matplotlib.font_manager as fm 
 from matplotlib import font_manager, rc
 
-font_family = "sans serif"
+font_family = "NANUMGOTHIC.TTF"
 plt.rcParams["font.family"] = font_family
 
 st.set_page_config(page_title="히트맵 시각화",layout="wide", page_icon="📊")
